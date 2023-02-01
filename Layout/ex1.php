57aad6c7b009c340d6e20a7b60937d959a5f4282
AndreiSusha <Andrey.Susha@gmail.com>
2023-02-01 11:29:37 +0200

    some changes

--- Layout/ex1.php
+++ Layout/ex1.php
@@ -1,4 +1,5 @@
-<?php include "header.php" ?>
+<?php
+ include "header.php" ?>
     <h3>3.1 Write a simple PHP script to print your information (Name and your groupid)</h3>
 <?php
 //opening tag
@@ -16,6 +17,38 @@ echo "Hello world! My name is \"Andrei\"";
 echo date("d.m.Y")
 ?>
 <h3>3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document</h3>
-<?php ?>
+
+<?php
+$title1 = "PHP is interesting";
+echo "<h1>" . $title1. "</h1>";
+?>
+
+
+<h3>3.5</h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+echo "
+<table>
+<tr>
+<th> S.N </th><th> Name </th><th> Grade </th>
+</tr>
+<tr>
+<td> 1 </td><td> Pekka </td> <td> $g1 </td>
+</tr>
+<tr>
+<td> 2 </td><td> Johanna </td> <td> $g2 </td>
+</tr>
+<tr>
+<td> 3 </td><td> John </td> <td> $g3 </td>
+</tr>
+</table>
+";
+?>
+
+<h3>4</h3>
+<img style="width: 100%" src="image.png" alt="i1"
+
 <?php include "footer.php" ?>
 
