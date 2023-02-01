33fbab99df33958e0301441fbc8c6a1c7d32717b
AndreiSusha <Andrey.Susha@gmail.com>
2023-02-01 10:29:16 +0200

    Initial commit

---
+++ Layout/ex1.php
@@ -0,0 +1,21 @@
+<?php include "header.php" ?>
+    <h3>3.1 Write a simple PHP script to print your information (Name and your groupid)</h3>
+<?php
+//opening tag
+echo "Andrei Susha<br>";
+echo "BBCAP22";
+?>
+<h3>3.2 Write PHP code to display the following message</h3>
+
+<?php
+echo "Hello world! My name is \"Andrei\"";
+?>
+<h3>3.3 Write the PHP code in to display the current date</h3>
+
+<?php
+echo date("d.m.Y")
+?>
+<h3>3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document</h3>
+
+<?php include "footer.php" ?>
+
